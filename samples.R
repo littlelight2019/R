11670c93c51575f64021c63ea539496a32de3def
littleLight2019 <q.jane.su@gmail.com>
2019-11-26 08:29:53 -0800

    add qqplot

--- samples.R
+++ samples.R
@@ -22,6 +22,7 @@ reject <- function(N, alpha = 0.05){
 # repeat sampling for B times and apply the same experiments to different sample sizes N, calculate power
 B = 2000; 
 Nseq = seq(5, 50, 5)
+
 power = sapply(Nseq, function(N) {r = replicate(B, reject(N)); mean(r)})
 plot(Nseq, power, main='Power by sample size', xlab='Sample size')
 lines(Nseq, power)
@@ -38,19 +39,21 @@ pvals = replicate(B, pval(25))
 hist(pvals)
 
 # tstat simulations
+B=2000
 tstat <- function(N){
   s = rnorm(N)
   mean(s) / sd(s) * sqrt(N)
 }
-qqcheck <- function(B=2000, N){
+qqcheck <- function(N){
   tstats = replicate(B, tstat(N))
   qqnorm(tstats)
   abline(0,1)
+  for(i in 1:1000000){
+  }
+  print(N)
+  N
 }
 sapply(seq(5, 50, 5), qqcheck)
-
-
-
 
 
 # large tstat simulation: tstat > 2 vs what's in t-distribution
@@ -61,7 +64,7 @@ tstatl <- function(N, cutoff = 2){
 }
 MCportion <- function(N, cutoff=2){
   large = replicate(B, tstatl(N))
-  mean(large)
+  mean(large) 
 }
 Ns = seq(5, 50, 5)
 MCportions = sapply(Ns, MCportion)
@@ -70,6 +73,26 @@ plot(Tportions, MCportions, ylim=c(0.02,0.07), xlim=c(0.02,0.07))
 abline(0,1)
 names(MCportions) <- Ns
 MCportions
+names(Tportions) <- Ns
+Tportions
+
+
+# qqplot
+dat = read.csv('femaleControlsPopulation.csv')
+tstat <- function(N){
+  #s1 = sample(dat[,1], N)
+  #s2 = sample(dat[,1], N)
+  s1 = rnorm(N)
+  s2 = rnorm(N)
+  (mean(s1) - mean(s2)) / sqrt(var(s1) / N + var(s2) / N)
+}
+N=10; B=2000
+MCtstats = replicate(B, tstat(N))
+qqnorm(MCtstats)
+Ps = seq(1/(B+1), B/(B+1), len=B)
+Ts = qt(Ps, df=2*N -1)
+qqplot(Ts, MCtstats)
+abline(0,1)
 
 
 # chance of getting a 6 from 1:100 dice: abs(Z) > 2
@@ -86,4 +109,6 @@ mean(abs(Zs) > 2)
 
 qqnorm(Zs)
 abline(0,1)
+
+#
 
